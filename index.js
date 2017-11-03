69f6d515bfa83592f8e6632b85eb4dc0fefd153b
Mats Ljunggren <mljunggren@gmail.com>
2017-11-02 20:10:36 -0400

    Added package files

--- index.js
+++ index.js
@@ -1,76 +1,49 @@
-const CDP = require('chrome-remote-interface');
-const argv = require('minimist')(process.argv.slice(2));
-const file = require('fs');
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
 
-// CLI Args
-const url = argv.url || 'https://www.google.com';
-const format = argv.format === 'jpeg' ? 'jpeg' : 'png';
-const viewportWidth = argv.viewportWidth || 1440;
-const viewportHeight = argv.viewportHeight || 900;
-const delay = argv.delay || 0;
-const userAgent = argv.userAgent;
-const fullPage = argv.full;
+const puppeteer = require('puppeteer');
+var fs = require('fs');
 
-// Start the Chrome Debugging Protocol
-CDP(async function(client) {
-  // Extract used DevTools domains.
-  const {DOM, Emulation, Network, Page, Runtime} = client;
+if (process.argv.length < 4) {
+  console.log('Usage: node test.js testurl reportfile');
+  process.exit(-1);
+}
 
-  // Enable events on domains we are interested in.
-  await Page.enable();
-  await DOM.enable();
-  await Network.enable();
+var testUrl = process.argv[2];
+var reportFile = process.argv[3];
+console.log('Accessing url: ' + testUrl);
 
-  // If user agent override was specified, pass to Network domain
-  if (userAgent) {
-    await Network.setUserAgentOverride({userAgent});
+(async() => {
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto(testUrl);
+await page.screenshot({path: reportFile + ".png"});
+
+page.on('console', (...args) => {
+  for (let i =0; i < args.length; ++i) {
+    
+    var logString = args[i];
+    console.log('Console output: ' + logString);
+    
+    if (logString.includes("<html>")) {
+      fs.writeFile(reportFile+".html", logString, function(err) {
+         if(err) {
+           return console.log(err);
+         }
+         console.log("The report was saved.");
+         }); 
+     }
+
+     if (logString.includes("All tests completed!")) { 
+        console.log("Tests completed. Waiting a few seconds to close browser so report is being sent.");
+        setTimeout(function(){
+          console.log("Closing browser.");
+          browser.close();
+        },6000);
+     }
   }
-
-  // Set up viewport resolution, etc.
-  const deviceMetrics = {
-    width: viewportWidth,
-    height: viewportHeight,
-    deviceScaleFactor: 0,
-    mobile: false,
-    fitWindow: false,
-  };
-  await Emulation.setDeviceMetricsOverride(deviceMetrics);
-  await Emulation.setVisibleSize({width: viewportWidth, height: viewportHeight});
-
-  // Navigate to target page
-  await Page.navigate({url});
-
-  // Wait for page load event to take screenshot
-  Page.loadEventFired(async () => {
-    // If the `full` CLI option was passed, we need to measure the height of
-    // the rendered page and use Emulation.setVisibleSize
-    if (fullPage) {
-      const {root: {nodeId: documentNodeId}} = await DOM.getDocument();
-      const {nodeId: bodyNodeId} = await DOM.querySelector({
-        selector: 'body',
-        nodeId: documentNodeId,
-      });
-      const {model: {height}} = await DOM.getBoxModel({nodeId: bodyNodeId});
-
-      await Emulation.setVisibleSize({width: viewportWidth, height: height});
-      // This forceViewport call ensures that content outside the viewport is
-      // rendered, otherwise it shows up as grey. Possibly a bug?
-      await Emulation.forceViewport({x: 0, y: 0, scale: 1});
-    }
-
-    setTimeout(async function() {
-      const screenshot = await Page.captureScreenshot({format});
-      const buffer = new Buffer(screenshot.data, 'base64');
-      file.writeFile('output.png', buffer, 'base64', function(err) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Screenshot saved');
-        }
-        client.close();
-      });
-    }, delay);
-  });
-}).on('error', err => {
-  console.error('Cannot connect to browser:', err);
 });
+
+})();
