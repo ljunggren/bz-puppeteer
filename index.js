1639b4f510e6023a9f8dc24eef68f33556ada4ea
Mats Ljunggren <mljunggren@gmail.com>
2019-01-15 23:53:27 -0500

    Added headfull support

--- index.js
+++ index.js
@@ -1,63 +1,42 @@
 #! /usr/bin/env node
+// Device Descriptors https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
 
-const puppeteer = require('puppeteer')
-const fs = require('fs')
+
+const puppeteer = require('puppeteer');
 const options = require('node-options');
+const fs = require('fs');
 
-var opts = {
-    device: "default",
-    timeout: null,
-    file: "boozang-" + new Date().toISOString().split('.')[0],
-    token: null,
-    screenshot: false
+
+const opts = {
+  "headfull": false,
+  "verbose" : false,
+  "file": "result",
+  "device" : "default",
+  "screenshot": false,
+  "token":""
 };
 
-var result = options.parse(process.argv.slice(2), opts);
+// Remove the first two arguments, which are the 'node' binary and the name
+// of your script.
+const result = options.parse(process.argv.slice(2), opts);
+const verbose = opts.verbose;
+const token = opts.token;
 
 if (result.errors) {
-    if (opts.verbose) console.log('Unknown argument(s): "' + result.errors.join('", "') + '"');
-    process.exit(2);
+  console.log('USAGE: boozang [--token] [--headfull] [--verbose] [--screenshot] [--file=report] [--device=default] [url]');
+  process.exit(-1);
 }
 
-if (result.errors || !result.args || result.args.length !== 1) {
-    console.log('USAGE: [--device=default] [--timeout=0] [--file=<boozang-Y-m-d>] [--token=<token_id>] [--screenshot] url');
-    process.exit(2);
+if (!result.args || result.args.length != 1 ){
+  console.log('USAGE: boozang [--token] [--headfull] [--verbose] [--screenshot] [--file=report] [--device=default] [url]');
+  process.exit(-2);
 }
 
-let url = result.args[0]
+const url = result.args.toString(); 
 
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-const isURL = (str) => {
-    const pattern = new RegExp('^(https?:\\/\\/)' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[.-a-z\\d_/]*)/run$', 'i') // mandatory fragment locator (. and / are allowed even if they are normally invalid)
-    return pattern.test(str)
-}
-
-if (typeof (url) == 'string' && !url.endsWith("/run")) {
-    if (!url.endsWith("/")) {
-        url += "/"
-    }
-    url += "run"
-}
-
-if (!url || !isURL(url)) {
-    console.error("Invalid URL: " + url)
-    process.exit(2)
-}
-
-if (opts.timeout) {
-    setTimeout(() => {
-        console.log(`Timeout! (${Number(opts.timeout)}s)`)
-        process.exit(2)
-    }, Number(opts.timeout) * 1000)
-}
+console.log('Opening URL: '+ url);
+console.log('Running with options: headfull=', opts.headfull, ', verbose=', opts.verbose, ', reportfile=', opts.reportfile, ', device=', opts.device);
+const file = opts.file;
 
 const RED = '\033[0;31m'
 const GREEN = '\033[0;32m'
@@ -83,47 +62,83 @@ const parseReport = (json) => {
 
 (async () => {
     if (url) {
-        let success = false
+  // Load extension if URL contains the word extension
+  const launchargs = getLaunchargs(url);
 
-        // Launch the browser
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--disable-infobars', '--no-sandbox', '--disable-setuid-sandbox']
-        })
+  function getLaunchargs(url){
+     if (url.includes('extension')) {
+      return [
+      '--disable-extensions-except=' + __dirname + '/bz-extension',
+      '--load-extension=' + __dirname + '/bz-extension',
+        ];
+     } else {
+      return [];
+     }
+  }
 
-        // Prepare the device
-        const page = await browser.newPage()
-        const devices = require('puppeteer/DeviceDescriptors')
+  if (launchargs.length > 0 && !opts.headfull) {
+    console.log("Url needs extension to run. Forcing headless mode to false.");
+  }
 
-        await page._client.send('Emulation.clearDeviceMetricsOverride')
-        if (opts.device === "default") {
-            console.log('Default device.')
-        } else if (!devices[opts.device]) {
-            console.log('Device ' + opts.device + ' not found, ignoring.')
-        } else {
-            console.log(`${opts.device} found.Viewport is set to ${devices[opts.device].viewport.width} x ${devices[opts.device].viewport.height}`)
-            await page.emulate(devices[opts.device])
-        }
+  const headlessMode = ! (opts.headfull || launchargs.length > 0)
+  
+  const browser = await puppeteer.launch({
+    headless: headlessMode,
+    args: launchargs 
+  });
 
-        console.log(`Opening URL: ${url}`)
 
-        // Insert token, if option set
-        if (opts.token && url.indexOf('token') == "-1") {
-            const position = url.indexOf('#')
-            url = [url.slice(0, position), (url.indexOf('?') == -1) ? '?' : '&', "token=" + opts.token, url.slice(position)].join('')
-        }
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-        await page.goto(url)
+function isURL(str) {
+  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
 
-        // Screenshot, if option enabled
-        if (opts.screenshot) {
-            console.log("Waiting two seconds for screenshot.")
-            await sleep(2000)
-            await page.screenshot({ path: `/var/boozang/${opts.file}.png` })
-            console.log(`Screenshot "${opts.file}.png" saved.`)
-        }
 
-        // Messages processing
+  let testUrl = url;
+
+  // Insert token if found in parameter.
+  if (token) {  
+    const position = url.indexOf('#');
+    testUrl = [url.slice(0, position), "&token=" + token, url.slice(position)].join('');
+    
+  } 
+
+  const page = await browser.newPage();
+  const devices = require('puppeteer/DeviceDescriptors');
+
+  await page._client.send('Emulation.clearDeviceMetricsOverride');
+  if (opts.device === "default") {
+   
+    console.log('No device specified.');
+  } else if (!devices[opts.device]) {
+    console.log('Device ' + opts.device + ' not found. Ignoring');
+  } else {
+    console.log(opts.device, 'found. Viewport is set to ',devices[opts.device].viewport.width,'x',devices[opts.device].viewport.height);
+    await page.emulate(devices[opts.device]);
+  }
+
+  console.log("Opening URL: " + testUrl);
+
+  await page.goto(testUrl);
+
+  if (opts.screenshot){
+    console.log("Wait a second for screenshot.");
+    await timeout(1000);
+    await page.screenshot({path: file + ".png"});
+    console.log("Closing browser.");
+    browser.close(); 
+  }
+
+  // Messages processing
         page.on('console', async msg => {
             let logString = msg.text()
 
