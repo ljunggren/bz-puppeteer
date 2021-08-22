e4e3bb09a9ce24a67b0f67776cefc655bed3f694
wensheng li <lws_home@hotmail.com>
2021-08-22 14:57:41 -0400

    socket:

--- bzSocket.js
+++ bzSocket.js
@@ -37,7 +37,7 @@ const BZSocket={
         }
         BZSocket.IP=BZSocket.IP.join(".")
         if(!opts.master){
-          BZSocket.PORT+=opts.key
+          BZSocket.PORT+=parseInt(opts.key)||0
         }
       }
     }
