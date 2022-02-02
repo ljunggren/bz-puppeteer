f5aade2fe6f77e2e699f200435ae2914ec17dc18
Wensheng Li <lws_home@hotmail.com>
2022-02-02 17:24:40 -0500

    rabbit:

--- logService.js
+++ logService.js
@@ -66,27 +66,29 @@ const Service = {
       });
 
       // Process queue
+      amqp.connect('amqp://localhost', function(error, connection) {
+        connection.createChannel(function(error, channel) {
+            var queue = 'task_queue';
 
-      connection.createChannel(function(error, channel) {
-          var queue = 'task_queue';
+            channel.assertQueue(queue, {
+                durable: true
+            });
+            channel.prefetch(1);
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+            channel.consume(queue, function(msg) {
+                var secs = msg.content.toString().split('.').length - 1;
 
-          channel.assertQueue(queue, {
-              durable: true
-          });
-          channel.prefetch(1);
-          console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-          channel.consume(queue, function(msg) {
-              var secs = msg.content.toString().split('.').length - 1;
+                console.log(" [x] Received %s", msg.content.toString());
+                setTimeout(function() {
+                    console.log(" [x] Done");
+                    channel.ack(msg);
+                }, secs * 1000);
+            }, {
+                noAck: false
+            });
+        });  
+      })
 
-              console.log(" [x] Received %s", msg.content.toString());
-              setTimeout(function() {
-                  console.log(" [x] Done");
-                  channel.ack(msg);
-              }, secs * 1000);
-          }, {
-              noAck: false
-          });
-      });  
     }
 
     this.logLevel=logLevel;
