32353ba913c424927d064bdafee5a2d8a401304d
ljunggren <mljunggren@gmail.com>
2022-03-12 17:26:41 -0500

    Added socket center code

---
+++ config/config.js
@@ -0,0 +1,7 @@
+
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Load the correct configuration file according to the 'NODE_ENV' variable
+module.exports = require('./env/' + process.env.NODE_ENV + '.js');
+console.log("NODE ENV" + process.env.NODE_ENV);
---
+++ config/env/development.js
@@ -0,0 +1,10 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Set the 'production' environment configuration object
+module.exports = {
+  DEFAULT_PROTOCOL:"http://",
+  SERVER_IP:"localhost",
+  PORT:80,
+  HTTPS_PORT:443
+};
