e291426bedcc11c5ef785467ff0800f16de1df2a
Wensheng Li <lws_home@hotmail.com>
2022-06-27 10:35:06 -0400

    rabbit:

--- logService.js
+++ logService.js
@@ -41,51 +41,51 @@ const Service = {
       // Assign queue
     
       amqp.connect('amqp://localhost', function(error0, connection) {
-          if (error0) {
-              throw error0;
+        if (error0) {
+          throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+          if (error1) {
+            throw error1;
           }
-          connection.createChannel(function(error1, channel) {
-              if (error1) {
-                  throw error1;
-              }
-              var queue = 'task_queue';
-              var msg = process.argv.slice(2).join(' ') || "Hello World!";
+          var queue = 'task_queue';
+          var msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-              channel.assertQueue(queue, {
-                  durable: true
-              });
-              channel.sendToQueue(queue, Buffer.from(msg), {
-                  persistent: true
-              });
-              console.log(" [x] Sent '%s'", msg);
+          channel.assertQueue(queue, {
+            durable: true
           });
-          setTimeout(function() {
-              connection.close();
-              process.exit(0);
-          }, 500);
+          channel.sendToQueue(queue, Buffer.from(msg), {
+            persistent: true
+          });
+          console.log(" [x] Sent '%s'", msg);
+        });
+        setTimeout(function() {
+          connection.close();
+          process.exit(0);
+        }, 500);
       });
 
       // Process queue
       amqp.connect('amqp://localhost', function(error, connection) {
         connection.createChannel(function(error, channel) {
-            var queue = 'task_queue';
+          var queue = 'task_queue';
 
-            channel.assertQueue(queue, {
-                durable: true
-            });
-            channel.prefetch(1);
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-            channel.consume(queue, function(msg) {
-                var secs = msg.content.toString().split('.').length - 1;
+          channel.assertQueue(queue, {
+            durable: true
+          });
+          channel.prefetch(1);
+          console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+          channel.consume(queue, function(msg) {
+            var secs = msg.content.toString().split('.').length - 1;
 
-                console.log(" [x] Received %s", msg.content.toString());
-                setTimeout(function() {
-                    console.log(" [x] Done");
-                    channel.ack(msg);
-                }, secs * 1000);
-            }, {
-                noAck: false
-            });
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function() {
+              console.log(" [x] Done");
+              channel.ack(msg);
+            }, secs * 1000);
+          }, {
+            noAck: false
+          });
         });  
       })
 
